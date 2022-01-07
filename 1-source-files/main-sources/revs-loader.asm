4a3b136ed2b144b8b4996621a74cefe55b7f247a
Mark Moxon <mark@moxon.net>
2022-01-07 12:32:28 +0000

    Disassembled relevant non-protection parts of loader

--- 1-source-files/main-sources/revs-loader.asm
+++ 1-source-files/main-sources/revs-loader.asm
@@ -44,27 +44,95 @@ CODE% = &2000           \ The address of the main game code
 \ ******************************************************************************
 
 ORG CODE%
+    CPU 1
+
+osbyte_flush_buffer_class = &0F
+
+L0000 = &0000
+L0080 = &0080
+L0100 = &0100
+L0400 = &0400
+L0500 = &0500
+L0600 = &0600
+L0700 = &0700
+L0CFF = &0CFF
+L0D00 = &0D00
+crtc_horz_total = &FE00
+crtc_horz_displayed = &FE01
+osbyte = &FFF4
+oscli = &FFF7
 
     ORG &2000
 
+.Start
 .pydis_start
-    EQUB &A9, &C9, &8D, 0  , &0D, &A2, 6  , &BD, &A8, &20, &9D, &FF   ; 2000: A9 C9 8D... ...
-    EQUB &0C, &CA, &D0, &F7, &A9, 4  , &85, &80, &A2, 0  , &A0, 0     ; 200C: 0C CA D0... ...
-    EQUB &CA, &D0, &FD, &88, &D0, &FA, &C6, &80, &D0, &F6, &A9, &0C   ; 2018: CA D0 FD... ...
-    EQUB &4C, 0  , &25, &A9, &0F, &A2, 0  , &A0, 0  , &20, &F4, &FF   ; 2024: 4C 00 25... L.%
-    EQUB &A9, &0B, &8D, 0  , &FE, &A9, 0  , &8D, 1  , &FE, &78, &A2   ; 2030: A9 0B 8D... ...
-    EQUB &FF, &9A, &A2, 0  , &B5, 0  , &9D, 0  , 7  , &BD, &AF, &20   ; 203C: FF 9A A2... ...
-    EQUB &9D, 0  , 4  , &BD, &AF, &21, &9D, 0  , 5  , &BD, &AF, &22   ; 2048: 9D 00 04... ...
-    EQUB &9D, 0  , 6  , &BD, &AF, &23, &95, 0  , &BD, &AF, &24, &9D   ; 2054: 9D 00 06... ...
-    EQUB 0  , 1  , &E8, &D0, &DB, &4C, 0  , 0                         ; 2060: 00 01 E8... ...
+    LDA #&C9                                                          ; 2000: A9 C9       ..
+    STA L0D00                                                         ; 2002: 8D 00 0D    ...
+    LDX #6                                                            ; 2005: A2 06       ..
+; &2007 referenced 1 time by &200E
+.loop_C2007
+    LDA L20A8,X                                                       ; 2007: BD A8 20    ..
+    STA L0CFF,X                                                       ; 200A: 9D FF 0C    ...
+    DEX                                                               ; 200D: CA          .
+    BNE loop_C2007                                                    ; 200E: D0 F7       ..
+    LDA #4                                                            ; 2010: A9 04       ..
+    STA L0080                                                         ; 2012: 85 80       ..
+    LDX #0                                                            ; 2014: A2 00       ..
+    LDY #0                                                            ; 2016: A0 00       ..
+; &2018 referenced 3 times by &2019, &201C, &2020
+.C2018
+    DEX                                                               ; 2018: CA          .
+    BNE C2018                                                         ; 2019: D0 FD       ..
+    DEY                                                               ; 201B: 88          .
+    BNE C2018                                                         ; 201C: D0 FA       ..
+    DEC L0080                                                         ; 201E: C6 80       ..
+    BNE C2018                                                         ; 2020: D0 F6       ..
+    LDA #&0C                                                          ; 2022: A9 0C       ..
+    JMP RunRevs                                                       ; 2024: 4C 00 25    L.%
+
+    LDA #osbyte_flush_buffer_class                                    ; 2027: A9 0F       ..
+    LDX #0                                                            ; 2029: A2 00       ..
+    LDY #0                                                            ; 202B: A0 00       ..
+    JSR osbyte                                                        ; 202D: 20 F4 FF     ..
+    LDA #&0B                                                          ; 2030: A9 0B       ..
+    STA crtc_horz_total                                               ; 2032: 8D 00 FE    ...
+    LDA #0                                                            ; 2035: A9 00       ..
+    STA crtc_horz_displayed                                           ; 2037: 8D 01 FE    ...
+    SEI                                                               ; 203A: 78          x
+    LDX #&FF                                                          ; 203B: A2 FF       ..
+    TXS                                                               ; 203D: 9A          .
+    LDX #0                                                            ; 203E: A2 00       ..
+; &2040 referenced 1 time by &2063
+.C2040
+    LDA L0000,X                                                       ; 2040: B5 00       ..
+    STA L0700,X                                                       ; 2042: 9D 00 07    ...
+    LDA L20AF,X                                                       ; 2045: BD AF 20    ..
+    STA L0400,X                                                       ; 2048: 9D 00 04    ...
+    LDA L21AF,X                                                       ; 204B: BD AF 21    ..!
+    STA L0500,X                                                       ; 204E: 9D 00 05    ...
+    LDA L22AF,X                                                       ; 2051: BD AF 22    .."
+    STA L0600,X                                                       ; 2054: 9D 00 06    ...
+    LDA L23AF,X                                                       ; 2057: BD AF 23    ..#
+    STA L0000,X                                                       ; 205A: 95 00       ..
+    LDA L24AF,X                                                       ; 205C: BD AF 24    ..$
+    STA L0100,X                                                       ; 205F: 9D 00 01    ...
+    INX                                                               ; 2062: E8          .
+    BNE C2040                                                         ; 2063: D0 DB       ..
+    JMP L0000                                                         ; 2065: 4C 00 00    L..
+
     EQUS "ACCESS SECURITY VIOLATION"                                  ; 2068: 41 43 43... ACC
     EQUB &0A, &0D                                                     ; 2081: 0A 0D       ..
-    EQUS "Protected by Toby Butler & Tim Sorrell"                     ; 2083: 50 72 6F... Pro
-    EQUB &A9, &4E, &8D, 0  , &7C, &40, &BB, &60, &C2, &CF, &12, &62   ; 20A9: A9 4E 8D... .N.
-    EQUB &FF, &E5, &92, 2  , &A7, &EF, &86, &A5, &E2, &93, &F2, &41   ; 20B5: FF E5 92... ...
-    EQUB &E2, &32, &92, &E3, 1  , &D5, &67, &9D, &61, &74, &F2, &25   ; 20C1: E2 32 92... .2.
-    EQUB &6E, &FE, &27, &D1, &62, &B4, &DE, &79, &B7, &47, &9E, &1F   ; 20CD: 6E FE 27... n.'
-    EQUB &82, &F5, &7E, &BB, &22, &94, &FD, &F7, &E2                  ; 20D9: 82 F5 7E... ..~
+    EQUS "Protected by Toby Butler & Tim Sorrel"                      ; 2083: 50 72 6F... Pro
+; &20A8 referenced 1 time by &2007
+.L20A8
+    EQUB &6C, &A9, &4E, &8D, 0  , &7C, &40                            ; 20A8: 6C A9 4E... l.N
+; &20AF referenced 1 time by &2045
+.L20AF
+    EQUB &BB, &60, &C2, &CF, &12, &62, &FF, &E5, &92, 2  , &A7, &EF   ; 20AF: BB 60 C2... .`.
+    EQUB &86, &A5, &E2, &93, &F2, &41, &E2, &32, &92, &E3, 1  , &D5   ; 20BB: 86 A5 E2... ...
+    EQUB &67, &9D, &61, &74, &F2, &25, &6E, &FE, &27, &D1, &62, &B4   ; 20C7: 67 9D 61... g.a
+    EQUB &DE, &79, &B7, &47, &9E, &1F, &82, &F5, &7E, &BB, &22, &94   ; 20D3: DE 79 B7... .y.
+    EQUB &FD, &F7, &E2                                                ; 20DF: FD F7 E2    ...
     EQUS "1wR"                                                        ; 20E2: 31 77 52    1wR
     EQUB &EF, &31, &1D, &B6, &86, &9F, &AF, &B5, &0E, &7F, &EF, &2F   ; 20E5: EF 31 1D... .1.
     EQUB &C6, &97, &3A, &76, &99, &2F, &9E, &1F, &82, &F7, &F7, &8A   ; 20F1: C6 97 3A... ..:
@@ -90,6 +158,8 @@ ORG CODE%
     EQUB &9A, 9  , &5F, &CF, &9F, &92, &8B, &32, &55, &0E, &D0, &D2   ; 2198: 9A 09 5F... .._
     EQUB &F2, &37, &D5, &97, &AA, &83, &DF                            ; 21A4: F2 37 D5... .7.
     EQUS "_z,F"                                                       ; 21AB: 5F 7A 2C... _z,
+; &21AF referenced 1 time by &204B
+.L21AF
     EQUB 0  , &3A, &F1, &0F, &FB, &56, &FE, &2F, &F6, &56, &FF, &7B   ; 21AF: 00 3A F1... .:.
     EQUB &ED, &7B, &EA, &7B, &EB, &56, &9E, &DE, &CB, &F9, &4D, &B1   ; 21BB: ED 7B EA... .{.
     EQUB &57, &E5, &98, &EE, &78, &EE, &54, &FD, &99, &E8, &79, &E8   ; 21C7: 57 E5 98... W..
@@ -119,16 +189,18 @@ ORG CODE%
     EQUS "Cil"                                                        ; 2299: 43 69 6C    Cil
     EQUB &FF, &4B, &16, &49, &16, 1                                   ; 229C: FF 4B 16... .K.
     EQUS "!TW"                                                        ; 22A2: 21 54 57    !TW
-    EQUB &EE, &79, &FE, &38, &1F, &4D, &FE, &F0, &81, &C0, &7A, &E9   ; 22A5: EE 79 FE... .y.
-    EQUB &36, 7  , &2F, &15, &56, &FF, &7A, &E6, &7B, &E2, &7B, &DD   ; 22B1: 36 07 2F... 6./
-    EQUB &DE, &B6, &FA, &4E, &B1, &1B, &FD, &78, &EB, &DD, &8C, &F9   ; 22BD: DE B6 FA... ...
-    EQUB &2D, &BB, &1B, &EB, &B8, &F4, &F6, &B9, &F1, &55, &F5, &79   ; 22C9: 2D BB 1B... -..
-    EQUB &EB, &59, &E6, &79, &E4, &5E, &E0, &7E, &DB, &5E, &E7, &7E   ; 22D5: EB 59 E6... .Y.
-    EQUB &DA, &DB, &E8, &FF, &4A, &E4, &DA, &9F, &FF, &2A, &E3, &DA   ; 22E1: DA DB E8... ...
-    EQUB &70, &FF, &5C, &FD, &7C, &F9, &5C, &FC, &7C, &F8, &5C, &FF   ; 22ED: 70 FF 5C... p.\
-    EQUB &7D, &FA, &5D, &FF, &7D, &FB, &94, &F7, &F8, &94, &F2, &F7   ; 22F9: 7D FA 5D... }.]
-    EQUB &52, &E8, &72, &ED, &52, &D7, &72, &EC, &52, &D6, &73, &EA   ; 2305: 52 E8 72... R.r
-    EQUB &D6, &9E, &F0, &BA, &E5, &F0                                 ; 2311: D6 9E F0... ...
+    EQUB &EE, &79, &FE, &38, &1F, &4D, &FE, &F0, &81, &C0             ; 22A5: EE 79 FE... .y.
+; &22AF referenced 1 time by &2051
+.L22AF
+    EQUB &7A, &E9, &36, 7  , &2F, &15, &56, &FF, &7A, &E6, &7B, &E2   ; 22AF: 7A E9 36... z.6
+    EQUB &7B, &DD, &DE, &B6, &FA, &4E, &B1, &1B, &FD, &78, &EB, &DD   ; 22BB: 7B DD DE... {..
+    EQUB &8C, &F9, &2D, &BB, &1B, &EB, &B8, &F4, &F6, &B9, &F1, &55   ; 22C7: 8C F9 2D... ..-
+    EQUB &F5, &79, &EB, &59, &E6, &79, &E4, &5E, &E0, &7E, &DB, &5E   ; 22D3: F5 79 EB... .y.
+    EQUB &E7, &7E, &DA, &DB, &E8, &FF, &4A, &E4, &DA, &9F, &FF, &2A   ; 22DF: E7 7E DA... .~.
+    EQUB &E3, &DA, &70, &FF, &5C, &FD, &7C, &F9, &5C, &FC, &7C, &F8   ; 22EB: E3 DA 70... ..p
+    EQUB &5C, &FF, &7D, &FA, &5D, &FF, &7D, &FB, &94, &F7, &F8, &94   ; 22F7: 5C FF 7D... \.}
+    EQUB &F2, &F7, &52, &E8, &72, &ED, &52, &D7, &72, &EC, &52, &D6   ; 2303: F2 F7 52... ..R
+    EQUB &73, &EA, &D6, &9E, &F0, &BA, &E5, &F0                       ; 230F: 73 EA D6... s..
     EQUS "_nx%"                                                       ; 2317: 5F 6E 78... _nx
     EQUB &8B, &57, &0A, &1D, &48, &10, &F3, &68, &74, &8A, &24, 3     ; 231B: 8B 57 0A... .W.
     EQUS "]sy\"                                                       ; 2327: 5D 73 79... ]sy
@@ -144,11 +216,13 @@ ORG CODE%
     EQUB &8A, &8F, &83, &84, &8D, &72, &EA, &B8, &8F, &9D, &83, &84   ; 238D: 8A 8F 83... ...
     EQUB &8D, &C9, &9D, &88, &99, &8C, &E9, &99, &C7, &AE, &AD, &0D   ; 2399: 8D C9 9D... ...
     EQUB &2F, &62, &FD, &12, &D6, &A0                                 ; 23A5: 2F 62 FD... /b.
-    EQUS "V+!2x"                                                      ; 23AB: 56 2B 21... V+!
-    EQUB &A9, &FF, &8D, &44, &FE, &A9, &91, &8D, &64, &FE, &A9, &F0   ; 23B0: A9 FF 8D... ...
-    EQUB &8D, &45, &FE, &A9, &45, &8D, &65, &FE, &20, &F0, 0  , &68   ; 23BC: 8D 45 FE... .E.
-    EQUB &4A, &80, &94, &52, &FE, &50, &C4, &FF, &F2, &41, &53, &BF   ; 23C8: 4A 80 94... J..
-    EQUB &F1, &4D, &87, &9A, &B8                                      ; 23D4: F1 4D 87... .M.
+    EQUS "V+!2"                                                       ; 23AB: 56 2B 21... V+!
+; &23AF referenced 1 time by &2057
+.L23AF
+    EQUB &78, &A9, &FF, &8D, &44, &FE, &A9, &91, &8D, &64, &FE, &A9   ; 23AF: 78 A9 FF... x..
+    EQUB &F0, &8D, &45, &FE, &A9, &45, &8D, &65, &FE, &20, &F0, 0     ; 23BB: F0 8D 45... ..E
+    EQUB &68, &4A, &80, &94, &52, &FE, &50, &C4, &FF, &F2, &41, &53   ; 23C7: 68 4A 80... hJ.
+    EQUB &BF, &F1, &4D, &87, &9A, &B8                                 ; 23D3: BF F1 4D... ..M
     EQUS "O28"                                                        ; 23D9: 4F 32 38    O28
     EQUB &80, &9B, &63, &C1, &F2, &D0, &7B, &E1, &5B, &54, &E9, &FC   ; 23DC: 80 9B 63... ..c
     EQUB 1  , &33, &86, &62, &C5, &77, &35, &D9, &F9, &42, &BC, 7     ; 23E8: 01 33 86... .3.
@@ -173,6 +247,8 @@ ORG CODE%
     EQUB &5C, &DC, &8C, &ED, &C1, &18                                 ; 249A: 5C DC 8C... \..
     EQUS "hHi"                                                        ; 24A0: 68 48 69    hHi
     EQUB 1  , &AA, &86, 0  , &B5, 0  , &20, 5  , 1  , &95, 0  , &E8   ; 24A3: 01 AA 86... ...
+; &24AF referenced 1 time by &205C
+.L24AF
     EQUB &E0, &F0, &D0, &F4, &60, &20, &0F, 1  , &4D, &45, &FE, &4D   ; 24AF: E0 F0 D0... ...
     EQUB &65, &FE                                                     ; 24BB: 65 FE       e.
     EQUS "`MD"                                                        ; 24BD: 60 4D 44    `MD
@@ -183,8 +259,14 @@ ORG CODE%
     EQUB 1  , &2C, &44, &FE, &30, &FB, &4D, &45, &FE, &20, &29, 1     ; 24E1: 01 2C 44... .,D
     EQUB &4D, &65, &FE, &4C, &29, 1                                   ; 24ED: 4D 65 FE... Me.
     EQUS "`L-"                                                        ; 24F3: 60 4C 2D    `L-
-    EQUB 1  , &60, 0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  , &A2, 7     ; 24F6: 01 60 00... .`.
-    EQUB &A0, &25, &4C, &F7, &FF                                      ; 2502: A0 25 4C... .%L
+    EQUB 1  , &60, 0  , 0  , 0  , 0  , 0  , 0  , 0  , 0               ; 24F6: 01 60 00... .`.
+; &2500 referenced 1 time by &2024
+.RunRevs
+    LDX #<(runRevs)                                                   ; 2500: A2 07       ..
+    LDY #>(runRevs)                                                   ; 2502: A0 25       .%
+    JMP oscli                                                         ; 2504: 4C F7 FF    L..
+
+.runRevs
     EQUS "*R.Revs?"                                                   ; 2507: 2A 52 2E... *R.
     EQUB &0D, 0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  , 0     ; 250F: 0D 00 00... ...
     EQUB 0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  , 0  , 0     ; 251B: 00 00 00... ...
@@ -201,6 +283,54 @@ ORG CODE%
     EQUB 0  , 0  , &3D, &15, &14, &15, 7  , &15, &FC, &14, &6E, &14   ; 259F: 00 00 3D... ..=
     EQUB &13, 9  , 0  , 0                                             ; 25AB: 13 09 00... ...
 .pydis_end
+
+; Label references by decreasing frequency:
+;     L0000:                 3
+;     C2018:                 3
+;     L0080:                 2
+;     L0100:                 1
+;     L0400:                 1
+;     L0500:                 1
+;     L0600:                 1
+;     L0700:                 1
+;     L0CFF:                 1
+;     L0D00:                 1
+;     loop_C2007:            1
+;     C2040:                 1
+;     L20A8:                 1
+;     L20AF:                 1
+;     L21AF:                 1
+;     L22AF:                 1
+;     L23AF:                 1
+;     L24AF:                 1
+;     RunRevs:               1
+;     crtc_horz_total:       1
+;     crtc_horz_displayed:   1
+;     osbyte:                1
+;     oscli:                 1
+
+; Automatically generated labels:
+;     C2018
+;     C2040
+;     L0000
+;     L0080
+;     L0100
+;     L0400
+;     L0500
+;     L0600
+;     L0700
+;     L0CFF
+;     L0D00
+;     L20A8
+;     L20AF
+;     L21AF
+;     L22AF
+;     L23AF
+;     L24AF
+;     loop_C2007
+    ASSERT <(runRevs) == &07
+    ASSERT >(runRevs) == &25
+    ASSERT osbyte_flush_buffer_class == &0F
 
 SAVE "3-assembled-output/Revs1.bin", pydis_start, pydis_end
 
